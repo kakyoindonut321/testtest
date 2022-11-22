c9633a630ec4d38cff3803a6cc022d750dec941b
kakyoindonut321 <forbiddenikki@gmail.com>
2022-11-22 10:31:54 +0700

    commit 1 ching chong

---
+++ resources/views/test.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>testetdttsteetstt</title>
+
+</head>
+<body>
+    <h1>ching chong ching cheng chong</h1>
+</body>
+</html>
